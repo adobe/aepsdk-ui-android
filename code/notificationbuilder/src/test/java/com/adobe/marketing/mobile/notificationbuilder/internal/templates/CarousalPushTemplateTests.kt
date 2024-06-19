213a4f7b7972686d1ab232402ce17ca02a47e49b
Navratan Soni <navratans@adobe.com>
2024-06-19 10:57:32 +0530

    Spotless apply

--- code/notificationbuilder/src/test/java/com/adobe/marketing/mobile/notificationbuilder/internal/templates/CarousalPushTemplateTests.kt
+++ code/notificationbuilder/src/test/java/com/adobe/marketing/mobile/notificationbuilder/internal/templates/CarousalPushTemplateTests.kt
@@ -1,6 +1,16 @@
+/*
+  Copyright 2024 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+*/
+
 package com.adobe.marketing.mobile.notificationbuilder.internal.templates
 
-import android.os.Bundle
 import com.adobe.marketing.mobile.notificationbuilder.PushTemplateConstants
 import com.adobe.marketing.mobile.notificationbuilder.internal.PushTemplateType
 import com.adobe.marketing.mobile.notificationbuilder.internal.templates.MockCarousalTemplateDataProvider.getMockedBundleWithAutoCarouselData
@@ -29,7 +39,7 @@ class CarousalPushTemplateTests {
         assertEquals(MOCKED_CAROUSEL_LAYOUT_DATA, template.rawCarouselItems)
     }
 
-   @Test
+    @Test
     fun `Test ManualCarouselPushTemplate initialization with Intent`() {
         val mockBundle = getMockedBundleWithManualCarouselData()
         val template = CarouselPushTemplate(IntentData(mockBundle, null))
@@ -50,7 +60,7 @@ class CarousalPushTemplateTests {
     @Test
     fun `Test ManualCarouselPushTemplate initialization with MapData`() {
         val mockedMap = getMockedMapWithManualCarouselData()
-        val template =  CarouselPushTemplate(MapData(mockedMap))
+        val template = CarouselPushTemplate(MapData(mockedMap))
         assertEquals(PushTemplateType.CAROUSEL, template.templateType)
         assertTrue(template is ManualCarouselPushTemplate)
         assertEquals(MOCKED_CAROUSEL_LAYOUT_DATA, template.rawCarouselItems)
